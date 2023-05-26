18d5481323e920cc5b05bce1b5d82f7271b29293
Aleksey Turchin <aleksey.turchin@nixsolutions.com>
2023-05-27 00:18:39 +0300

    initial commit 2

---
+++ src/scripts/game.ts
@@ -0,0 +1,23 @@
+import 'phaser'
+import MainScene from './scenes/mainScene'
+import PreloadScene from './scenes/preloadScene'
+
+const DEFAULT_WIDTH = 1300
+const DEFAULT_HEIGHT = 1300
+
+const config = {
+  type: Phaser.AUTO,
+  backgroundColor: '#000000',
+  scale: {
+    parent: 'app',
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: DEFAULT_WIDTH,
+    height: DEFAULT_HEIGHT
+  },
+  scene: [PreloadScene, MainScene],
+}
+
+window.addEventListener('load', () => {
+  const game = new Phaser.Game(config)
+})
